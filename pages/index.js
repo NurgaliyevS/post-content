d63f619fb77c96bec1ab884e06bebd7bc2cffbfb
NurgaliyevS <nurgasab@gmail.com>
2025-03-19 21:36:19 +0500

    upd

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
+import { customConfig } from "@/project.custom.config";
 
 export default function Home() {
   return (
     <>
       <Head>
+        <title>{customConfig.documentTitle}</title>
+        <meta name="description" content={customConfig.seo.description} />
+        <meta name="keywords" content={customConfig.seo.keywords} />
+        <meta name="theme-color" content={customConfig.seo.themeColor} />
+        <meta name="application-name" content={customConfig.seo.applicationName} />
         
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={customConfig.seo.og.url} />
+        <meta property="og:title" content={customConfig.seo.og.title} />
+        <meta property="og:description" content={customConfig.seo.description} />
+        <meta property="og:image" content={customConfig.seo.og.image} />
+        <meta property="og:image:alt" content={customConfig.seo.og.imageAlt} />
+        
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={customConfig.seo.og.twitterSite} />
+        <meta name="twitter:title" content={customConfig.seo.og.title} />
+        <meta name="twitter:description" content={customConfig.seo.description} />
+        <meta name="twitter:image" content={customConfig.seo.og.twitterImage} />
+        
+        <link rel="canonical" href={customConfig.domainWithHttps} />
       </Head>
       <div className="min-h-screen bg-base-100">
         {/* Navigation */}

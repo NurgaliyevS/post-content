f8172abc9e54ba5d022129c4b29ea6f483055073
NurgaliyevS <nurgasab@gmail.com>
2025-03-26 22:09:54 +0500

    upd

--- backend/user.js
+++ backend/user.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema(
   {
     name: String,
-    email: { type: String, unique: true },
+    email: { type: String},
     redditId: String,
     image: String,
     variant_name: { type: String, default: "free" }, // Updated

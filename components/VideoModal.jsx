64bda4b83d0b1d4b109dc2681ae08a313a116212
NurgaliyevS <nurgasab@gmail.com>
2025-03-28 15:30:09 +0500

    upd

--- components/VideoModal.jsx
+++ components/VideoModal.jsx
@@ -13,7 +13,7 @@ export default function VideoModal() {
         <div className="relative" style={{ paddingBottom: '56.25%' }}>
           <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/cqWBjQyynQU?autoplay=1"
+            src="https://www.youtube.com/embed/cqWBjQyynQU"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen

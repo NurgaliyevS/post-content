cc9a4b41d4dffcb2df31de7f5fa0b316b6cee612
NurgaliyevS <nurgasab@gmail.com>
2025-04-11 17:10:15 +0500

    upd

--- pages/api/create-checkout.js
+++ pages/api/create-checkout.js
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
 
     const createLink = {
       mode: "subscription",
+      allow_promotion_codes: true,
       custom_fields: [
         {
           key: "reddit_username",

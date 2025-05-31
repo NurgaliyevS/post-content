5c192a2fa4879968303396acd3e8f0161016f5b1
NurgaliyevS <nurgasab@gmail.com>
2025-05-31 16:07:40 +0500

    upd

--- pages/affiliates.js
+++ pages/affiliates.js
@@ -1,13 +1,18 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Affiliates() {
-  const [referrals, setReferrals] = useState(1);
+  const [referrals, setReferrals] = useState(10);
 
   // Calculate earnings based on referrals
   const effectiveReferrals = referrals || 1;
-  const earnings = effectiveReferrals * 35;
+  const earnings = effectiveReferrals * 17.5;
+
+  const handleBecomeAnAffiliate = () => {
+    window.open("https://post-content.affonso.io", "_blank");
+  };
 
   return (
     <Layout>
@@ -15,18 +20,35 @@ export default function Affiliates() {
         <title>Affiliates - Post Content</title>
         <meta
           name="description"
-          content="Earn by referring developers to Post Content."
+          content="Earn while you sleep with affiliate commissions on Post Content."
         />
       </Head>
-      <main className="min-h-screen flex flex-col items-center justify-center px-4 py-16">
-        <section className="w-full max-w-3xl text-center">
+      <main className="flex flex-col items-center justify-center px-4 py-16">
+        <section className="text-center px-4 py-16 mx-auto">
           <h1 className="text-4xl md:text-6xl font-extrabold mb-6">
+            Increase your revenue with affiliate commissions
+          </h1>
+          <p className="text-base md:text-lg text-gray-600 mb-8">
+            Make 50% of every sale you invite
+          </p>
+          <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+            <button
+              className="btn btn-primary btn-wide md:w-48"
+              onClick={handleBecomeAnAffiliate}
+            >
+              <FaArrowRight className="w-3 h-3" />
+              Become an affiliate
+            </button>
+          </div>
+        </section>
+        <section className="w-full max-w-3xl text-center">
+          <h2 className="text-2xl md:text-4xl font-extrabold mb-6">
             You can make{" "}
             <span className="bg-black text-white px-4 py-1 rounded-md">
               ${earnings}
             </span>{" "}
             today
-          </h1>
+          </h2>
           <p className="text-xl mb-16">
             And you would help <span className="font-bold">{referrals}</span>{" "}
             creators turn their side projects into full-time businesses
@@ -38,7 +60,7 @@ export default function Affiliates() {
               max="100"
               value={referrals || 0}
               onChange={(e) => setReferrals(Number(e.target.value))}
-              className="range w-full range-secondary"
+              className="range w-full"
             />
             <span class="text-lg">
               <span class="font-bold">{referrals}</span> referrals

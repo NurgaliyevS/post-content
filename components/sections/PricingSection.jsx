15a93ce01698210d2ad69b7f5eb0921ad64711d3
NurgaliyevS <nurgasab@gmail.com>
2025-06-09 21:28:15 +0500

    upd

--- components/sections/PricingSection.jsx
+++ components/sections/PricingSection.jsx
@@ -5,13 +5,14 @@ export default function PricingSection() {
   const [loading, setLoading] = useState(false);
   const [activeTooltip, setActiveTooltip] = useState(null);
 
-  const referralId = typeof window !== "undefined" ? window.affonso_referral : null;
+  const referralId =
+    typeof window !== "undefined" ? window.affonso_referral : null;
 
   const handleSubscription = async (plan) => {
     try {
       // VISIT WEBSITE WITH REFERRAL ID
       console.log("referralId", referralId);
-      
+
       setLoading(true);
       const response = await fetch("/api/create-checkout", {
         method: "POST",
@@ -46,6 +47,11 @@ export default function PricingSection() {
         post_available: 100,
         price: 3500, // in cents
       },
+      growth: {
+        name: "Growth",
+        post_available: 50,
+        price: 2500, // in cents
+      },
     };
     return details[plan];
   };
@@ -53,31 +59,92 @@ export default function PricingSection() {
   return (
     <section id="pricing" className="py-16 px-4 mx-auto">
       <h2 className="text-3xl font-bold text-center mb-12">Pricing</h2>
-      <div className="grid md:grid-cols-2 gap-8 mx-auto">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto">
+        {/* Growth Plan */}
+        <div className="card bg-base-100 border">
+          <div className="card-body relative">
+            <h3 className="card-title">Growth</h3>
+            <div className="text-3xl font-bold mb-2">
+              $25<span className="text-sm text-gray-500">/month</span>
+            </div>
+            <span className="text-sm text-gray-500 mb-4">
+              Perfect for solopreneurs ready to learn Reddit marketing and
+              build their first audience.
+            </span>
+            <ul className="space-y-2">
+              <li className="flex items-center gap-2">
+                <span>✓</span> 50 posts per month
+              </li>
+              <li className="flex items-center gap-2">
+                <span>✓</span> Schedule posts on the best time
+              </li>
+              <li className="flex items-center gap-2">
+                <span>✓</span> Cross-posting to relevant subreddits
+              </li>
+              <li className="flex items-center gap-2 relative">
+                <span>✓</span> Growth consulting{" "}
+                <button
+                  className="inline-flex"
+                  onMouseEnter={() => setActiveTooltip("growth")}
+                  onMouseLeave={() => setActiveTooltip(null)}
+                >
+                  <FaInfoCircle className="text-gray-400" />
+                </button>
+                {activeTooltip === "growth" && (
+                  <div className="absolute left-0 bottom-full mb-2 p-3 bg-gray-800 text-white text-sm rounded w-64 z-10">
+                    After buying this plan, you will get automatically email
+                    with a link to schedule a call with us. But it does not stop
+                    here, we will help you with:
+                    <ul className="mt-2">
+                      <li>
+                        <span>✓</span> Content creation
+                      </li>
+                      <li>
+                        <span>✓</span> Outreach to customers
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </li>
+            </ul>
+
+            <button
+              onClick={() => handleSubscription("growth")}
+              disabled={loading}
+              className="btn btn-outline mt-4 btn-wide md:w-full"
+            >
+              <FaArrowRight className="w-3 h-3" />
+              {loading ? "Loading..." : "Get Started"}
+            </button>
+          </div>
+        </div>
 
         {/* Hypergrowth Plan */}
-        <div className="card bg-base-100 border">
-          <div className="card-body">
+        <div className="card bg-base-100 border border-primary">
+          <div className="card-body relative">
             <h3 className="card-title">Hypergrowth</h3>
             <div className="text-3xl font-bold mb-2">
               $35<span className="text-sm text-gray-500">/month</span>
             </div>
             <span className="text-sm text-gray-500 mb-4">
-              For founders who want to grow on Reddit and have the time to do it themselves.
+              For ambitious founders who want to grow on Reddit while building business.
             </span>
             <ul className="space-y-2">
               <li className="flex items-center gap-2">
                 <span>✓</span> 100 posts per month
               </li>
               <li className="flex items-center gap-2">
-                <span>✓</span> Schedule posts
+                <span>✓</span> Schedule posts on the best time
               </li>
               <li className="flex items-center gap-2">
-                <span>✓</span> Cross-posting
+                <span>✓</span> Cross-posting to relevant subreddits
               </li>
               <li className="flex items-center gap-2 relative">
-                <span>✓</span> Hypergrowth consulting{" "}
-                <button 
+                <span>✓</span> 
+                <p className="text-nowrap">
+                  Hypergrowth consulting{" "}
+                  </p>
+                <button
                   className="inline-flex"
                   onMouseEnter={() => setActiveTooltip("hypergrowth")}
                   onMouseLeave={() => setActiveTooltip(null)}
@@ -86,9 +153,10 @@ export default function PricingSection() {
                 </button>
                 {activeTooltip === "hypergrowth" && (
                   <div className="absolute left-0 bottom-full mb-2 p-3 bg-gray-800 text-white text-sm rounded w-64 z-10">
-                    After bying this plan, you will get automatically email with a link to schedule a call with us.
-                    But it does not stop here, we will help you with:
-                    <ul>
+                    After buying this plan, you will get automatically email
+                    with a link to schedule a call with us. But it does not stop
+                    here, we will help you with:
+                    <ul className="mt-2">
                       <li>
                         <span>✓</span> Content creation
                       </li>
@@ -109,7 +177,7 @@ export default function PricingSection() {
             <button
               onClick={() => handleSubscription("hypergrowth")}
               disabled={loading}
-              className="btn btn-outline mt-4 btn-wide md:w-full"
+              className="btn btn-primary mt-4 btn-wide md:w-full"
             >
               <FaArrowRight className="w-3 h-3" />
               {loading ? "Loading..." : "Get Started"}
@@ -117,15 +185,15 @@ export default function PricingSection() {
           </div>
         </div>
 
-
-        <div className="card bg-base-100 border border-primary">
+        {/* Reddit Partner Plan */}
+        <div className="card bg-base-100 border">
           <div className="card-body relative">
             <h3 className="card-title">Reddit Partner</h3>
             <div className="text-3xl font-bold mb-2">
               $1000<span className="text-sm text-gray-500">/month</span>
             </div>
             <span className="text-sm text-gray-500 mb-4">
-              For busy founders who want to grow on Reddit but don't have the time to do it themselves.
+              For busy founders who want to dominate on Reddit but don't have the time.
             </span>
             <ul className="space-y-2">
               <li className="flex items-center gap-2">
@@ -144,7 +212,7 @@ export default function PricingSection() {
             <button
               onClick={() => handleSubscription("redditPartner")}
               disabled={loading}
-              className="btn btn-primary mt-4 btn-wide md:w-full"
+              className="btn btn-outline mt-4 btn-wide md:w-full"
             >
               <FaArrowRight className="w-3 h-3" />
               {loading ? "Loading..." : "Get Started"}
